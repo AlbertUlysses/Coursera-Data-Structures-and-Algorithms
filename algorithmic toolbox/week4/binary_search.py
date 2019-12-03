80792be179eef8b61a940f5d99c45d1d940e25af
AlbertUlysses <albertulysseschavez@gmail.com>
2019-12-02 22:08:30 -0800

    bug: fix index error

--- algorithmic toolbox/week4/binary_search.py
+++ algorithmic toolbox/week4/binary_search.py
@@ -2,7 +2,7 @@
 import sys
 
 def binary_search(a, x):
-    left, right = 0, len(a)
+    left, right = 0, len(a) - 1
     # write your code here
     while left <= right:
         mid = (left + right)//2
