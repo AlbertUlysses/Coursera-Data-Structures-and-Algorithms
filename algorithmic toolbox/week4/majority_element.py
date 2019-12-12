a5c3663b8ba329b627e2deedfc9bb1354915b1ef
AlbertUlysses <albertulysseschavez@gmail.com>
2019-12-12 09:15:32 -0800

    bug: correct code

--- algorithmic toolbox/week4/majority_element.py
+++ algorithmic toolbox/week4/majority_element.py
@@ -7,8 +7,23 @@ def get_majority_element(a, left, right):
     if left + 1 == right:
         return a[left]
     #write your code here
-    return -1
+    left_m = get_majority_element(a, left, (left + right - 1)//2 + 1)
+    right_m = get_majority_element(a, (left + right - 1)//2 + 1, right)
+    left_count = 0
+    for i in range(left, right):
+        if a[i] == left_m:
+            left_count += 1
+    if left_count > (right-left)//2:
+        return left_m
 
+    right_count = 0
+    for i in range(left, right):
+        if a[i] == right_m:
+            right_count += 1
+    if right_count > (right-left)//2:
+        return right_m
+
+    return -1
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
